b53e667cbb2c5f92bf9a8bca81d31afe52837284
Prashanth Balakrishnan <prashbala13@gmail.com>
2023-09-12 14:14:36 -0400

    store complete

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,23 @@
+import getBillboard from "@/actions/getBillboard";
+import getProducts from "@/actions/getProducts";
+import Billboard from "@/components/Billboard";
+import ProductList from "@/components/ProductList";
+import Container from "@/components/ui/Container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const billboard = await getBillboard("701a5520-0adc-4e9d-9a7e-fc49d6025370");
+  const products = await getProducts({ isFeatured: true });
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
+      </div>
+    </Container>
+  );
+};
+export default HomePage;
